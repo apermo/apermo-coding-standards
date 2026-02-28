7dbfb9803f5198f1eb535b1e1cd87e85f7543af0
Christoph Daum <christoph.daum@coding-pioneers.com>
2026-02-28 11:47:37 +0100

    feat(rules): require trailing comma in calls
    Enable SlevomatCodingStandard.Functions
    .RequireTrailingCommaInCall for multi-line
    function calls.
    
    Closes #61

--- tests/Integration/RulesetIntegrationTest.php
+++ tests/Integration/RulesetIntegrationTest.php
@@ -212,6 +212,12 @@ class RulesetIntegrationTest extends TestCase {
 		$this->assertNoErrorsOnLine( $file, 12, 'Concat at start of line should be allowed.' );
 	}
 
+	public function testRequireTrailingCommaInCall(): void {
+		$file = $this->processFixture( 'TrailingCommaInCall.inc' );
+		$this->assertErrorOnLine( $file, 9, 'RequireTrailingCommaInCall', 'Missing trailing comma should be flagged.' );
+		$this->assertNoErrorsOnLine( $file, 15, 'Trailing comma should be allowed.' );
+	}
+
 	public function testNullTypeHintOnLastPosition(): void {
 		$file = $this->processFixture( 'NullTypeHintPosition.inc' );
 		$this->assertErrorOnLine( $file, 12, 'NullTypeHintOnLastPosition', 'null|string should be flagged.' );
